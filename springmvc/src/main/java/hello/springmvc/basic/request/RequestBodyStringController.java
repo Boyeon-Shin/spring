9e24867bad20985ef48aa26c02343d6e886efee6
Boyeon-Shin <anna007096@gmail.com>
2025-04-04 21:17:50 +0900

    7. 스프링 MVC - 기본 기능

---
+++ springmvc/src/main/java/hello/springmvc/basic/request/RequestBodyStringController.java
@@ -0,0 +1,26 @@
+package hello.springmvc.basic.request;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletInputStream;
+import jakarta.servlet.ServletOutputStream;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.StreamUtils;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Slf4j
+@Controller
+public class RequestBodyStringController {
+
+    @PostMapping("/request-body-string-v1")
+    public void requestBodyString(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        ServletInputStream inputStream = request.getInputStream();
+        String messageBody = StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);
+
+        
+    }
+}
