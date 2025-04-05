3956d25a6cf457c4a372b89e72ebea0517884f58
Boyeon-Shin <anna007096@gmail.com>
2025-04-05 19:05:13 +0900

    7. 스프링 MVC - 기본 기능

--- springmvc/src/main/java/hello/springmvc/basic/request/RequestBodyStringController.java
+++ springmvc/src/main/java/hello/springmvc/basic/request/RequestBodyStringController.java
@@ -2,25 +2,58 @@ package hello.springmvc.basic.request;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.ServletInputStream;
-import jakarta.servlet.ServletOutputStream;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.Writer;
 import java.nio.charset.StandardCharsets;
+import lombok.Locked.Write;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.StreamUtils;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Slf4j
 @Controller
 public class RequestBodyStringController {
 
     @PostMapping("/request-body-string-v1")
-    public void requestBodyString(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    public void requestBodyString(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
         ServletInputStream inputStream = request.getInputStream();
         String messageBody = StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);
 
-        
+        log.info(messageBody);
+
+        response.getWriter().write("ok");
+    }
+
+    @PostMapping("/request-body-string-v2")
+    public void requestBodyStringV2(InputStream inputStream,
+                                    Writer responseWriter) throws ServletException, IOException {
+        String messageBody = StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);
+        log.info(messageBody);
+        responseWriter.write("ok");
+    }
+
+
+    @PostMapping("/request-body-string-v3")
+    public HttpEntity<String> requestBodyStringV3(HttpEntity<String> httpEntity) throws IOException {
+        String body = httpEntity.getBody();
+        log.info("messageBody= {} ", body);
+        return new HttpEntity<>("ok");
+    }
+
+
+    @PostMapping("/request-body-string-v4")
+    public String requestBodyStringV3(@RequestBody String messageBody) throws IOException {
+        log.info("messageBody= {}", messageBody);
+        return "ok";
     }
 }
